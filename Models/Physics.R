2b9b5404b23a9302b23e39b15096fe9a1ad9e77b
Robert Ladwig <ladwigjena@gmail.com>
2022-11-05 13:13:06 -0500

    Update Physics.R

--- Models/Physics.R
+++ Models/Physics.R
@@ -8,6 +8,7 @@ source('src/integralenergy_functions.R')
 #library(LakeModelR)
 library(dplyr)
 library(readr)
+library(chron)
 
 aquatic_sites <- read_csv("https://raw.githubusercontent.com/eco4cast/neon4cast-targets/main/NEON_Field_Site_Metadata_20220412.csv") |>
   dplyr::filter(aquatics == 1)
